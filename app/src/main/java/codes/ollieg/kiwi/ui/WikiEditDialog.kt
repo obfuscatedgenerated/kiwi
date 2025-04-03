76435875bd55fb66bc69af9be6468fa5dc59c055
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-04-03 21:49:27 +0100

    edit dialog top bar

--- app/src/main/java/codes/ollieg/kiwi/ui/WikiEditDialog.kt
+++ app/src/main/java/codes/ollieg/kiwi/ui/WikiEditDialog.kt
@@ -1,19 +1,29 @@
 package codes.ollieg.kiwi.ui
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,10 +43,56 @@ fun WikiEditDialog(
         shape = RectangleShape, // gets rid of the rounded corners to make it look more natural
         dragHandle = null, // as well as hiding the drag handle
     ) {
-        Surface (
-            modifier = Modifier.safeContentPadding().fillMaxSize()
-        ) {
-            Text("Edit Wiki Dialog - $wikiId")
+        Scaffold (
+            modifier = Modifier.safeDrawingPadding().fillMaxSize().padding(horizontal = 8.dp),
+            topBar = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // separate boxes for left and right floated content
+                    Row (
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = onDismissRequest) {
+                            Icon(
+                                Icons.Outlined.Close,
+                                contentDescription = "Close modal",
+                            )
+                        }
+
+                        // if wikiId is null, this is an add wiki dialog
+                        val title = if (wikiId == null) {
+                            "Add wiki"
+                        } else {
+                            "Edit wiki"
+                        }
+
+                        Text(
+                            text = title,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
+
+                    // right floated content
+                    Row (
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Button(
+                            onClick = { /* TODO: save wiki */ },
+                            modifier = Modifier.padding(16.dp),
+                            colors = ButtonDefaults.textButtonColors()
+                        ) {
+                            Text("Save")
+                        }
+                    }
+                }
+            }
+        ) { padding ->
+
         }
     }
 }
