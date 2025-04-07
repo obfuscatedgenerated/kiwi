cf0013f71762da572fd9e3d3b59a35fab610d3d7
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-04-07 19:25:24 +0100

    try to auth

--- app/src/main/java/codes/ollieg/kiwi/data/ApiUtil.kt
+++ app/src/main/java/codes/ollieg/kiwi/data/ApiUtil.kt
@@ -13,10 +13,6 @@ import io.ktor.client.request.request
 import io.ktor.client.request.setBody
 import io.ktor.http.HttpMethod
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
 import java.net.URL
 import java.net.URLEncoder
 
--- app/src/main/java/codes/ollieg/kiwi/ui/ScreenWikiHome.kt
+++ app/src/main/java/codes/ollieg/kiwi/ui/ScreenWikiHome.kt
@@ -11,15 +11,26 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import codes.ollieg.kiwi.AppScreens
+import codes.ollieg.kiwi.data.isLoggedInToMediawiki
+import codes.ollieg.kiwi.data.logInToMediawiki
 import codes.ollieg.kiwi.data.room.ArticlesViewModel
 import codes.ollieg.kiwi.data.room.WikisViewModel
+import io.ktor.util.valuesOf
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,6 +49,58 @@ fun ScreenWikiHome(
 
     if (wiki.value == null) {
         return
+    }
+
+    // log into the api if required
+    val authRequired = wiki.value!!.authUsername != "" && wiki.value!!.authPassword != ""
+    var authenticated by remember { mutableStateOf<Boolean?>(false) }
+
+    // if wiki changes, check authentication again
+    LaunchedEffect(wiki.value) {
+        authenticated = false
+
+        if (authRequired) {
+            Log.i("ScreenWikiHome", "Auth details found for ${wiki.value!!.name}, logging in...")
+
+            CoroutineScope(Dispatchers.IO).launch {
+                if (isLoggedInToMediawiki(wiki.value!!.apiUrl, wiki.value!!.authUsername)) {
+                    Log.i(
+                        "ScreenWikiHome",
+                        "Already logged in to ${wiki.value!!.name}. No action needed."
+                    )
+                } else {
+                    Log.i("ScreenWikiHome", "Logging in to ${wiki.value!!.name}...")
+
+                    try {
+                        logInToMediawiki(
+                            wiki.value!!.apiUrl,
+                            wiki.value!!.authUsername,
+                            wiki.value!!.authPassword
+                        )
+                        authenticated = true
+                    } catch (e: Exception) {
+                        Log.e("ScreenWikiHome", "Error logging in to ${wiki.value!!.name}: ${e.message}")
+                        authenticated = null
+                    }
+                }
+            }
+        } else {
+            Log.i("ScreenWikiHome", "No auth details found for ${wiki.value!!.name}. No action needed.")
+            authenticated = true
+        }
+    }
+
+    // wait for authentication to finish
+    if (authenticated == false) {
+        return CenteredLoader()
+    }
+
+    // if authentication fails, show error message
+    if (authenticated == null) {
+        return Text(
+            text = "Error logging in to ${wiki.value!!.name}. Please check your credentials.",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 
     val starredArticles = articlesViewModel.getStarredByWikiLive(wiki.value!!).observeAsState()
