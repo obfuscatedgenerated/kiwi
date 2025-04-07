228b9c46521cfd7259c3585e1ada9fa73431e165
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-04-07 16:32:52 +0100

    use listitem in wikilist

--- app/src/main/java/codes/ollieg/kiwi/ui/ButtonWikiStorageDelete.kt
+++ app/src/main/java/codes/ollieg/kiwi/ui/ButtonWikiStorageDelete.kt
@@ -1,14 +1,11 @@
 package codes.ollieg.kiwi.ui
 
 import android.util.Log
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import codes.ollieg.kiwi.data.room.Wiki
 
 @Composable
@@ -20,8 +17,7 @@ fun ButtonWikiStorageDelete(
         onClick = {
             Log.i("ButtonWikiStorageDelete", "Delete button clicked for ${wiki.name}")
             onClick(wiki)
-        },
-        modifier = Modifier.padding(horizontal = 16.dp),
+        }
     ) {
         Icon(
             imageVector = Icons.Outlined.Delete,
--- app/src/main/java/codes/ollieg/kiwi/ui/WikiList.kt
+++ app/src/main/java/codes/ollieg/kiwi/ui/WikiList.kt
@@ -1,17 +1,14 @@
 package codes.ollieg.kiwi.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import codes.ollieg.kiwi.data.room.Wiki
 
 @Composable
@@ -21,39 +18,37 @@ fun WikiList(
     subtexts: List<String>? = null,
     button: @Composable ((Wiki) -> Unit)? = null,
 ) {
-    LazyColumn (
+    LazyColumn(
         modifier = modifier.fillMaxWidth()
     ) {
         items(wikis.size) { index ->
             val wiki = wikis[index]
             val subtext = subtexts?.getOrNull(index)
 
-            // TODO: adapt to listitem for consistency
-            Row(
-                modifier = Modifier.padding(vertical = 4.dp).fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Column (
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    verticalArrangement = Arrangement.Center,
-                ) {
+            ListItem(
+                modifier = Modifier.fillMaxWidth(),
+                colors = ListItemDefaults.colors(
+                    containerColor = Color.Transparent
+                ),
+                headlineContent = {
                     Text(
                         text = wiki.name,
                     )
-
+                },
+                supportingContent = {
                     if (subtext != null) {
                         Text(
                             text = subtext,
                             style = MaterialTheme.typography.bodySmall
                         )
                     }
+                },
+                trailingContent = {
+                    if (button != null) {
+                        button(wiki)
+                    }
                 }
-
-                if (button != null) {
-                    button(wiki)
-                }
-            }
+            )
         }
     }
 }
