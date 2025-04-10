9b1a5dafe26e43ac851b1785e7d6c5a8162da241
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-04-10 20:22:06 +0100

    paragraph and heading parsing

--- app/src/main/java/codes/ollieg/kiwi/ui/ScreenArticle.kt
+++ app/src/main/java/codes/ollieg/kiwi/ui/ScreenArticle.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
@@ -16,6 +18,8 @@ import androidx.compose.ui.unit.sp
 import codes.ollieg.kiwi.R
 import codes.ollieg.kiwi.data.room.ArticlesViewModel
 import codes.ollieg.kiwi.data.room.WikisViewModel
+
+val SECTION_DEBUG = false
 
 @Composable
 fun ScreenArticle(
@@ -49,18 +53,72 @@ fun ScreenArticle(
 
         // TODO: make headings bigger based on number of equals (could try annotated string?)
 
-        // double any newline characters to make paragraphs more readable
-        // TODO: split paragraphs into separate text elements to make lazy loading more efficient
-        parsed = parsed.replace("\n", "\n\n")
+        val sections = parsed.split("\n\n")
 
         LazyColumn {
-            item {
-                Text(
-                    text = parsed,
-                    modifier = Modifier.padding(16.dp),
-                    lineHeight = 24.sp, // more readable TODO: configurable
-                    fontFamily = FontFamily.Serif // TODO: connect to preference system
-                )
+            items(sections.size) { index ->
+                val section = sections[index]
+                val paragraphs = section.split("\n")
+
+                // render paragraphs separately for efficient loading and clean separation
+                for (paragraph in paragraphs) {
+                    if (paragraph.isEmpty()) continue
+
+                    var text = paragraph
+                    var textStyle = MaterialTheme.typography.bodyLarge
+                    var bottomPadding = 16.dp
+                    var topPadding = 16.dp
+
+                    // if the text of this paragraph is in the format =^n (.*) =^n then it is a heading format
+                    val strippedParagraph = paragraph.replace("\n", "")
+                    val isHeading = Regex("^=+ (.*) =+$").matches(strippedParagraph)
+
+                    if (isHeading) {
+                        // count the number of equals signs and strip them
+                        // this assumes the number of equals signs is the same on both sides, which it should be from textextracts
+                        val headingLevel = strippedParagraph.indexOf(" ")
+                        text = strippedParagraph.substring(headingLevel + 1, strippedParagraph.length - headingLevel - 1)
+
+                        // determine the textstyle to apply based on the heading level
+                        // we can skip h1 as it is not usually added to the text (used as title) but still just treat it as h2
+                        textStyle = when (headingLevel) {
+                            1, 2 -> MaterialTheme.typography.headlineLarge
+                            3 -> MaterialTheme.typography.headlineMedium
+                            4 -> MaterialTheme.typography.headlineSmall
+                            5 -> MaterialTheme.typography.titleLarge
+                            6 -> MaterialTheme.typography.titleMedium
+                            else -> MaterialTheme.typography.titleSmall
+                        }
+
+                        // disable bottom padding for headings, and set top padding based on level
+                        bottomPadding = 0.dp
+                        topPadding = when (headingLevel) {
+                            1 -> 32.dp
+                            2 -> 24.dp
+                            else -> 16.dp
+                        }
+
+                        // TODO: connect to preference system
+                    }
+
+                    Text(
+                        text = text,
+                        modifier = Modifier.padding(bottom = bottomPadding, top = topPadding, start = 16.dp, end = 16.dp),
+                        lineHeight = 24.sp, // more readable TODO: configurable
+                        fontFamily = FontFamily.Serif, // TODO: connect to preference system
+                        fontSize = textStyle.fontSize,
+                    )
+
+                    if (SECTION_DEBUG) {
+                        // new paragraph
+                        HorizontalDivider()
+                    }
+                }
+
+                if (SECTION_DEBUG) {
+                    // new section
+                    HorizontalDivider(color = MaterialTheme.colorScheme.primary)
+                }
             }
         }
     }
