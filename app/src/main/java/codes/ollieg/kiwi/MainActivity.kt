9d88b0c44d8e1b273dc326cfb062212bf86bdbf7
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-04-22 17:07:23 +0100

    use modern network approach + add broadcast reciever

--- app/src/main/java/codes/ollieg/kiwi/MainActivity.kt
+++ app/src/main/java/codes/ollieg/kiwi/MainActivity.kt
@@ -1,16 +1,18 @@
 package codes.ollieg.kiwi
 
-import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresPermission
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
@@ -32,59 +34,121 @@ enum class AppScreens {
     OtherSettings,
 }
 
-class ConnectionChangeReceiver : BroadcastReceiver() {
-    var lastOnlineValue: Boolean? = null
+class ConnectionChangeReceiver(context: Context) {
+    var initialValue: Boolean? = null
+    var lastAirplaneMode: Boolean = false
 
-    fun setInitialValue(context: Context) {
-        // set initial value for lastOnlineValue
-        lastOnlineValue = checkOnline(context)
-        Log.i("ConnectionChangeReceiver", "Initial connection state: $lastOnlineValue")
+    fun setInitialValues(context: Context) {
+        this.initialValue = checkOnline(context)
+        this.lastAirplaneMode = isAirplaneModeOn(context)
     }
 
-    @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
-    override fun onReceive(context: Context, intent: Intent) {
-        val isOnline = checkOnline(context)
-        Log.i("ConnectionChangeReceiver", "Connection state event: $isOnline")
+    private fun isAirplaneModeOn(context: Context): Boolean {
+        return Settings.Global.getInt(context.contentResolver, Settings.Global.AIRPLANE_MODE_ON, 0) == 1
+    }
 
-        if (isOnline == lastOnlineValue) {
-            // no change in connection state, do nothing
-            Log.i("ConnectionChangeReceiver", "No change in connection state, ignoring event.")
-            return
+
+    private val connectionChangeCallback = object : ConnectivityManager.NetworkCallback() {
+        // network is available for use
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+
+            if (initialValue == true) {
+                // ignore this event once, as sometimes the callback recieves an event as soon as the app starts
+                initialValue = null
+                return
+            }
+
+            // if it was airplane mode that changed, ignore this event and let the broadcast receiver handle it
+            if (isAirplaneModeOn(context) != lastAirplaneMode) {
+                lastAirplaneMode = isAirplaneModeOn(context)
+                return
+            }
+
+            Toast.makeText(context, context.getString(R.string.back_online), Toast.LENGTH_LONG)
+                .show()
         }
 
-        lastOnlineValue = isOnline
+        // lost network connection
+        override fun onLost(network: Network) {
+            super.onLost(network)
 
-        if (!isOnline) {
-            Toast.makeText(context, context.getString(R.string.device_offline), Toast.LENGTH_LONG).show()
-        } else {
-            Toast.makeText(context, context.getString(R.string.back_online), Toast.LENGTH_LONG).show()
+            if (initialValue == false) {
+                // ignore this event once, as sometimes the callback recieves an event as soon as the app starts
+                initialValue = null
+                return
+            }
+
+            // if it was airplane mode that changed, ignore this event and let the broadcast receiver handle it
+            if (isAirplaneModeOn(context) != lastAirplaneMode) {
+                lastAirplaneMode = isAirplaneModeOn(context)
+                return
+            }
+
+            Toast.makeText(context, context.getString(R.string.device_offline), Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    fun getCallback(): ConnectivityManager.NetworkCallback {
+        return connectionChangeCallback
+    }
+}
+
+class AirplaneModeReciever : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_AIRPLANE_MODE_CHANGED) {
+            val isAirplaneModeOn = intent.getBooleanExtra("state", false)
+            if (isAirplaneModeOn) {
+                Toast.makeText(context, context.getString(R.string.airplane_mode_on), Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                Toast.makeText(context, context.getString(R.string.airplane_mode_off), Toast.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 }
 
 class MainActivity : ComponentActivity() {
-    var connChangeReceiver = ConnectionChangeReceiver()
+    val networkRequest: NetworkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        .addTransportType(NetworkCapabilities.TRANSPORT_VPN)
+        .addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
+        .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        .build()
+
+    val connChangeReceiver = ConnectionChangeReceiver(this)
+    val networkCallback = connChangeReceiver.getCallback()
+
+    val airplaneModeReceiver = AirplaneModeReciever()
 
     override fun onResume() {
         super.onResume()
 
-        // register receiver to show toast when device connects to or loses connection to the internet
-        // TODO: use newer callback for this, but use a different receiver to get the marks (maybe just do a diff message if they set airplane mode)
+        // register receiver for airplane mode changes
         val connChangeReceiverFlags = ContextCompat.RECEIVER_NOT_EXPORTED
-        val connChangeFilter = IntentFilter("android.net.conn.CONNECTIVITY_CHANGE")
-        ContextCompat.registerReceiver(this, connChangeReceiver, connChangeFilter, connChangeReceiverFlags)
+        val connChangeFilter = IntentFilter("android.intent.action.AIRPLANE_MODE")
+        ContextCompat.registerReceiver(this, airplaneModeReceiver, connChangeFilter, connChangeReceiverFlags)
+
+        // set network callback
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
     }
 
     override fun onPause() {
         super.onPause()
 
         // unregister receiver to avoid memory leaks
-        unregisterReceiver(connChangeReceiver)
+        unregisterReceiver(airplaneModeReceiver)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        connChangeReceiver.setInitialValue(this.applicationContext)
+
+        // now we can access system services, so set the initial value of the connection change receiver
+        connChangeReceiver.setInitialValues(this)
 
         setContent {
             val navController = rememberNavController()
