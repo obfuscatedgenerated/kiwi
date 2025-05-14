cc2961ea64de5f4bfc5fd6827315f1b230ceae98
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-05-14 17:20:21 +0100

    sdk version looks good

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "codes.ollieg.kiwi"
-        minSdk = 24 // TODO: change this
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
